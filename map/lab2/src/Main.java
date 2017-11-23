a3a9cf3e3d2d42e22fcf278f8d5b26d34fab4a01
rexu <tigrex0806@gmail.com>
2017-11-23 19:06:24 +0200

    tried javafx

--- map/lab2/src/Main.java
+++ map/lab2/src/Main.java
@@ -1,11 +1,25 @@
 import domain.Grade;
 import domain.Project;
 import domain.Student;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import repository.*;
 import service.Service;
 import ui.Console;
 
 public class Main {
+//    @Override
+//    public void start(Stage stage) {
+//        Group root = new Group();
+//        Scene scene = new Scene(root, 550, 500);
+//        stage.setTitle("Title");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+
     public static void main(String[] args) {
         Repository<Student, Integer> stRepo = new StudentFileRepository(new StudentValidator(), "students.txt");
         Repository<Project, Integer> prRepo = new ProjectFileRepository(new ProjectValidator(), "projects.txt");
@@ -14,5 +28,6 @@ public class Main {
         Service s = new Service(stRepo, prRepo, grRepo);
         Console c = new Console(s);
         c.run();
+//        launch(args);
     }
 }
