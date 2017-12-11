19ca0e9dbdd13e4405e8786d796259c9a4a2d8a9
rexu <tigrex0806@gmail.com>
2017-12-11 18:30:21 +0200

    Finish lab5

--- plf/lab5/c.lisp
+++ plf/lab5/c.lisp
@@ -1,4 +1,28 @@
-(defun sortare (l)
-	(sort (remove-duplicates l) #'<))
+(defun elim (l e)
+	(cond
+		((null l) nil)
+		((= (car l) e) (elim (cdr l) e))
+		(t (cons (car l) (elim (cdr l) e)))
+	)
+)
 
-(write (sortare '(3 6 1 4 6)))
+(defun min2 (l m)
+	(cond
+		((null l) m)
+		((>= (car l) m) (min2 (cdr l) m))
+		(t (min2 (cdr l) (car l)))
+	)
+)
+
+(defun sortare (l)
+	(cond
+		((null l) nil)
+		((null (cdr l)) (list (car l)))
+		(t (cons (min2 l (car l)) (sortare (elim l (min2 l (car l))))))
+	)
+)
+
+(write (sortare '(6 5 4 3 2 1)))
+(write-line "")
+(write (sortare '(1 5 3 7 5 9 7 9)))
+(write-line "")
--- plf/lab5/d.lisp
+++ plf/lab5/d.lisp
@@ -1,28 +1,17 @@
-(defun in (l1 e)
-	(if (null l1)
-		0)
-	(if (= (car l1) e)
-		1
-	)
-	(if (and (not (null l1)) (\= (car l1) e))
-	    (in (cdr l1) e))
-)
-
-(defun in2 (l e)
+(defun in (l e)
 	(cond
 		((null l) 0)
 		((= (car l) e) 1)
-		(T (in2 (cdr l) e))
+		(T (in (cdr l) e))
 	)
 )
 
 (defun inter (l1 l2)
-	(if (null l1)
-		nil
-		)
-	(if (= (in l2 (car l1)) 1)
-		(cons (car l1) (inter (cdr l1) l2))
-		(inter (cdr l1) l2)))
+	(cond
+		((null l1) nil)
+		((= (in l2 (car l1)) 1) (cons (car l1) (inter (cdr l1) l2)))
+		(t (inter (cdr l1) l2))
+	)
+)
 
-
-(write (in '(1 2 3 4) 4))
+(write (inter '(1 2 3 4 4 5 7 6) '(1 2 3 6 8 9 0)))
