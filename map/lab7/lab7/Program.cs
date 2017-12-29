5721db08e49315c2966e955c85e8e136b2814ad0
Rexu <hateshi.naito@gmail.com>
2017-12-29 17:27:54 +0200

    meow meow

---
+++ map/lab7/lab7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab7
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] names = { "Burke", "Connor", "Frank", 
+                "Everett", "Albert", "George", 
+                "Harris", "David" };
+            
+            IEnumerable<string> query = from s in names 
+                where s.Length == 5
+                orderby s
+                select s.ToUpper();
+
+            query.ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
