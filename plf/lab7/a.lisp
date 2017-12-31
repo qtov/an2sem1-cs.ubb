2f1deb7f9c424d24d872d857976ffc7ff0f5a2f5
rexu <tigrex0806@gmail.com>
2017-12-31 16:04:03 +0200

    lab7 plf

---
+++ plf/lab7/a.lisp
@@ -0,0 +1,9 @@
+(defun adancime (l)
+	(cond
+		((null l) 1)
+		((atom l) 0)
+		(t (+ 1 (apply #'max (mapcar #'adancime l))))
+	)
+)
+
+(write (adancime '(1 1 1 1 (2 2 (3 3 3 (4 4 (5 5 5 5) 4 4) 3) 2 (3 (4 4 4 (5 5 (6 6 (7 7 7 7) 6) 5 5) 4) 3) 2) 1 1 1)))
